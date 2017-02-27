fce139d5607d4add772c31be0c8faf990a5469e3
Kurt <knikaitani@gmail.com>
2017-02-26 18:55:55 -1000

    Task 2: Tasklist setup

---
+++ app/tasklist.js
@@ -0,0 +1,16 @@
+/**
+ * Created by knika on 2/26/2017.
+ */
+Tasks = new Mongo.Collection('tasks');
+
+if (Meteor.isClient) {
+  Template.tasks.helpers({
+    tasks: function() {
+      return Tasks.find({}, {sort: {createdAt: -1}});
+    }
+  });
+}
+
+if (Meteor.isServer) {
+
+}
